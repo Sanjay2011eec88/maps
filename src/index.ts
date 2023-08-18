43cf8197e2b231c8a48d68e9f8e95ed6bd49c3fb
Sanjay2011eec88 <sanjaykumar.varma099@gmail.com>
2023-08-18 06:12:09 +0000

    Creating a custom map class

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
-import { User } from "./User";
-import {Company} from "./Company";
-const user = new User();
-console.log(user);
-const company = new Company();
-console.log(company);
+// import { User } from "./User";
+// import {Company} from "./Company";
+import { CustomMap } from "./CustomMap";
+// const user = new User();
+// console.log(user);
+// const company = new Company();
+// console.log(company);
+new CustomMap()
